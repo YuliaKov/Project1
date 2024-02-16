d3e0b59fc37205647272f862a3455a590e75684c
YuliaKov <yulia.kovalchuk90.com>
2024-02-16 19:23:11 +0200

    adaptive 768

--- assets/js/main_js/main.js
+++ assets/js/main_js/main.js
@@ -1,10 +1,18 @@
 $(function () {
-  $(".projects-slider").slick({
+  $(".slider").slick({
     arrows: true,
     dots: true,
     infinite: true,
     speed: 300,
     slidesToShow: 4,
     slidesToScroll: 2,
+    responsive: [
+      {
+        breakpoint: 769,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+    ],
   });
 });
