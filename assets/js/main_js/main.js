c9a81a042db73f23f9fd27a8a938ff87ad732e92
YuliaKov <yulia.kovalchuk90.com>
2024-02-17 23:10:04 +0200

    added burger on index

--- assets/js/main_js/main.js
+++ assets/js/main_js/main.js
@@ -16,3 +16,8 @@ $(function () {
     ],
   });
 });
+
+document.querySelector(".burger").addEventListener("click", function () {
+  this.classList.toggle("active");
+  document.querySelector(".header-wrapper").classList.toggle("open");
+});
