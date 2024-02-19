db64b319ce63d3b2557a47df75e48d71519d282d
YuliaKov <yulia.kovalchuk90.com>
2024-02-19 14:12:16 +0200

    added adwords

---
+++ assets/js/adwords_js/adwords.js
@@ -0,0 +1,10 @@
+$(function () {
+  $('.containerstages__items').slick({
+    arrows: true,
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 2,
+  });
+});
